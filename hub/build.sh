a1441c1894e91111c16518f5f4f5669352d1acd8
rasmusmunk <munk1@live.dk>
2018-03-12 00:15:28 +0100

    Fixed pytest support and test execution permissions

--- hub/build.sh
+++ hub/build.sh
@@ -3,4 +3,4 @@ IMAGE_NAME=nielsbohr/jupyterhub
 IMAGE_VERS=edge
 
 docker build -t $IMAGE_NAME:$IMAGE_VERS .
-docker build -t $IMAGE_NAME:$IMAGE_VERS -f Dockerfile.test .
+docker build -t $IMAGE_NAME:$IMAGE_VERS-test -f Dockerfile.test .
